2af3caf8bb83de9e646621045d66d5567a6c9093
Kai Geffen <geffenkai@gmail.com>
2023-03-06 16:35:03 -0500

    Added some images, made header fade how I want

--- script/script.js
+++ script/script.js
@@ -1,14 +1,9 @@
 // add class navbarDark on navbar scroll
-const header = document.querySelector('.navbar');
-console.log(header)
-window.onscroll = function() {
-    const top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.remove('navbarDark');
-    }
+const header = document.querySelector('.navbarDark')
+
+window.onscroll = () => {
+    const top = window.scrollY
+    header.style.backgroundColor = `rgba(0, 0, 0, ${top/100})`
 }
 // collapse navbar after click on small devices
 const navLinks = document.querySelectorAll('.nav-item')
