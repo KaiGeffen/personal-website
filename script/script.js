a2ea2c8869ddb893e2c721bff1eadbc5d8bcda55
Kai Geffen <geffenkai@gmail.com>
2023-03-14 20:40:17 -0400

    Added new hero image, adjusted header js to trigger on load as well, make text fade white > black as background fades in

--- script/script.js
+++ script/script.js
@@ -1,10 +1,19 @@
 // add class navbarDark on navbar scroll
 const header = document.querySelector('.navbarDark')
+const header_text = document.querySelectorAll('.header-text')
 
-window.onscroll = () => {
+function refreshHeader() {
     const top = window.scrollY
     header.style.backgroundColor = `rgba(0, 0, 0, ${top/100})`
+    header_text.forEach(element => {
+        const val = top * 255 / 100
+        element.style.color = `rgb(${val}, ${val}, ${val})`
+    })
 }
+
+window.onscroll = refreshHeader
+window.onload = refreshHeader
+
 // collapse navbar after click on small devices
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarSupportedContent')
